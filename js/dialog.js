de1576f8c7c49ecebb228cfe715a4c9ef0aa73f4
Nikita Vorobev <xlopok188@gmail.com>
2019-07-29 20:21:55 +0400

    разбил на модули

--- js/dialog.js
+++ js/dialog.js
@@ -1,55 +1,56 @@
 'use strict';
+(function () {
+  var setup = document.querySelector('.setup');
+  var dialogHandler = setup.querySelector('.upload');
 
-var setup = document.querySelector('.setup');
-var dialogHandler = setup.querySelector('.upload');
 
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-dialogHandler.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
-
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-
-  var dragged = false;
-
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-    dragged = true;
-
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
+    var dragged = false;
 
-    setup.style.top = (setup.offsetTop - shift.y) + 'px';
-    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
 
-  };
-
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-
-    if (dragged) {
-      var onClickPreventDefault = function (evtClick) {
-        evtClick.preventDefault();
-        dialogHandler.removeEventListener('click', onClickPreventDefault);
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
       };
-      dialogHandler.addEventListener('click', onClickPreventDefault);
-    }
-  };
 
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
 
-});
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
 
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (evtClick) {
+          evtClick.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+  });
+
+})();
--- js/stat.js
+++ js/stat.js
@@ -1,44 +1,46 @@
 'use strict';
+(function () {
+  var CLOUD_WIDTH = 420;
+  var CLOUD_HEIGHT = 270;
+  var CLOUD_X = 100;
+  var CLOUD_Y = 10;
+  var GAP = 10;
+  var COLUMNGAP = 50;
+  var BAR_WIDTH = 40;
+  var barHeight = 150;
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var GAP = 10;
-var COLUMNGAP = 50;
-var BAR_WIDTH = 40;
-var barHeight = 150;
+  var renderCloud = function (ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  };
 
-var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-};
+  window.renderStatistics = function (ctx, players, times) {
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+    var maxElement = Math.max.apply(null, times); // Наибольшее число в массиве
 
-  var maxElement = Math.max.apply(null, times); // Наибольшее число в массиве
+    // Поздравительный текст
+    ctx.fillStyle = '#000000';
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + 30);
+    ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + 50);
 
-  // Поздравительный текст
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + 30);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + 50);
+    for (var i = 0; i < players.length; i++) {
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+      ctx.fillText(players[i], (COLUMNGAP + CLOUD_X) + (COLUMNGAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
 
-  for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(players[i], (COLUMNGAP + CLOUD_X) + (COLUMNGAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
+      if (players[i] === 'Вы') {
+        ctx.fillstyle = 'red';
+      } else {
+        var randomOpacity = Math.random();
+        ctx.fillStyle = 'rgba(0, 0, 255,' + randomOpacity + ')';
+      }
+      ctx.fillRect((COLUMNGAP + CLOUD_X) + (COLUMNGAP + BAR_WIDTH) * i, (CLOUD_HEIGHT - GAP * 3) - ((barHeight * times[i]) / maxElement), BAR_WIDTH, (barHeight * times[i]) / maxElement);
 
-    if (players[i] === 'Вы') {
-      ctx.fillstyle = 'red';
-    } else {
-      var randomOpacity = Math.random();
-      ctx.fillStyle = 'rgba(0, 0, 255,' + randomOpacity + ')';
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+      ctx.fillText(Math.round(times[i]), (COLUMNGAP + CLOUD_X) + (COLUMNGAP + BAR_WIDTH) * i, (CLOUD_HEIGHT - GAP * 3) - ((barHeight * times[i]) / maxElement) - GAP);
     }
-    ctx.fillRect((COLUMNGAP + CLOUD_X) + (COLUMNGAP + BAR_WIDTH) * i, (CLOUD_HEIGHT - GAP * 3) - ((barHeight * times[i]) / maxElement), BAR_WIDTH, (barHeight * times[i]) / maxElement);
+  };
 
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(Math.round(times[i]), (COLUMNGAP + CLOUD_X) + (COLUMNGAP + BAR_WIDTH) * i, (CLOUD_HEIGHT - GAP * 3) - ((barHeight * times[i]) / maxElement) - GAP);
-  }
-};
+})();
